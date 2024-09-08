4a8a1c4cd6b5740c703ca163510d7550a1b597fe
JudahJL <judahlegy@gmail.com>
2024-09-08 23:21:20 +0530

    fixed invisible weather EditorID(now requires po3_tweaks as optional dependency)

--- src/menus/Trainer.cpp
+++ src/menus/Trainer.cpp
@@ -1,3 +1,4 @@
+
 #include "imgui.h"
 // #include "imgui_internal.h"
 // #include <imgui_impl_dx11.h>
@@ -14,7 +15,7 @@ namespace World {
             const auto calendar{ RE::Calendar::GetSingleton() };
             if(calendar) [[likely]] {
                 if(float* ptr{ &(calendar->gameHour->value) }; ptr) [[likely]] {
-                    ImGui::SliderFloat("Time", ptr, 0.0F, 24.0F);
+                    ImGui::SliderFloat("Time", ptr, 0.0f, 24.f);
                     _sliderActive = ImGui::IsItemActive();
                     return;
                 }
@@ -24,13 +25,13 @@ namespace World {
     }  // namespace Time
 
     namespace Weather {
-        RE::TESRegion* _currRegionCache = nullptr;  // after fw `region` field of sky gets reset. restore from this.
+        RE::TESRegion* _currRegionCache{ nullptr };  // after fw `region` field of sky gets reset. restore from this.
 
         RE::TESRegion* getCurrentRegion() {
             const auto sky{ RE::Sky::GetSingleton() };
             if(sky) [[likely]] {
-                auto ret{ sky->region };
-                if(ret) [[likely]] {
+                auto* ret{ sky->region };
+                if(ret) {
                     _currRegionCache = ret;
                     return ret;
                 }
@@ -39,35 +40,36 @@ namespace World {
         }
 
         // maps to store formEditorID as names for regions and weathers as they're discarded by bethesda
-        std::unordered_map<RE::TESRegion*, std::string>  _regionNames;
-        std::unordered_map<RE::TESWeather*, std::string> _weatherNames;
+        std::unordered_map<const RE::TESRegion*, std::string>  _regionNames;
+        std::unordered_map<const RE::TESWeather*, std::string> _weatherNames;
 
         std::vector<RE::TESWeather*>     _weathersToSelect;
-        static std::vector<RE::TESFile*> _mods;  // plugins containing weathers
+        // static std::vector<const RE::TESFile*> _mods;  // plugins containing weathers
         // static uint8_t _mods_i = 0;              // selected weather plugin
-        bool                             _cached = false;
+        bool                             _cached{ false };
 
-        bool _showCurrRegionOnly = true;  // only show weather corresponding to current region
+        bool _showCurrRegionOnly{ true };  // only show weather corresponding to current region
 
-        bool _lockWeather = false;
+        bool _lockWeather{ false };
 
-        static std::vector<std::pair<std::string, bool>> _filters = {
-            {           "Pleasant", false },
-            {             "Cloudy", false },
-            {              "Rainy", false },
-            {               "Snow", false },
-            {   "Permanent aurora", false },
-            { "Aurora follows sun", false }
+        static std::array _filters{
+            std::make_pair("Pleasant", false),
+            std::make_pair("Cloudy", false),
+            std::make_pair("Rainy", false),
+            std::make_pair("Snow", false),
+            std::make_pair("Permanent aurora", false),
+            std::make_pair("Aurora follows sun", false)
         };
 
         void cache() {
             _weathersToSelect.clear();
-            auto regionDataManager{ RE::TESDataHandler::GetSingleton()->GetRegionDataManager() };
+            const auto TESDataHandler{ RE::TESDataHandler::GetSingleton() };
+            const auto regionDataManager{ TESDataHandler->GetRegionDataManager() };
             if(!regionDataManager) [[unlikely]] {
                 return;
             }
-            for(auto weather : RE::TESDataHandler::GetSingleton()->GetFormArray<RE::TESWeather>()) {
-                if(!weather) [[unlikely]] {
+            for(auto weather : TESDataHandler->GetFormArray<RE::TESWeather>()) {
+                if(!weather) {
                     continue;
                 }
                 auto flags = weather->data.flags;
@@ -103,12 +105,12 @@ namespace World {
                 }
                 if(_showCurrRegionOnly) {
                     bool belongsToCurrRegion{ false };
-                    auto currRegion{ getCurrentRegion() };
+                    auto currRegion          { getCurrentRegion() };
                     if(currRegion) {  // could've cached regionData -> weathers at the cost of a bit of extra space, but this runs fine in O(n2) since there's limited # of weathers
                         for(RE::TESRegionData* regionData : currRegion->dataList->regionDataList) {
                             if(regionData->GetType() == RE::TESRegionData::Type::kWeather) {
-                                RE::TESRegionDataWeather* weatherData = regionDataManager->AsRegionDataWeather(regionData);
-                                for(auto t : weatherData->weatherTypes) {
+                                RE::TESRegionDataWeather* weatherData{ regionDataManager->AsRegionDataWeather(regionData) };
+                                for(const auto* t : weatherData->weatherTypes) {
                                     if(t->weather == weather) {
                                         belongsToCurrRegion = true;
                                     }
@@ -127,7 +129,7 @@ namespace World {
         void show() {
             RE::TESRegion*  currRegion{ nullptr };
             RE::TESWeather* currWeather{ nullptr };
-            const auto      sky{ RE::Sky::GetSingleton() };
+            const auto sky{ RE::Sky::GetSingleton() };
             if(sky) [[likely]] {
                 currRegion  = getCurrentRegion();
                 currWeather = sky->currentWeather;
@@ -138,10 +140,8 @@ namespace World {
                 for(auto* i : _weathersToSelect) {
                     const bool isSelected = (i == currWeather);
                     if(ImGui::Selectable(_weatherNames[i].c_str(), isSelected)) {
-                        if(!isSelected) {
-                            if(sky) [[likely]] {
-                                sky->ForceWeather(i, true);
-                            }
+                        if(!isSelected && sky) {
+                            sky->ForceWeather(i, true);
                         }
                     }
                     if(isSelected) {
@@ -157,7 +157,7 @@ namespace World {
             ImGui::Text("Flags:");
 
             for(int i = 0; i < _filters.size(); i++) {
-                if(ImGui::Checkbox(_filters[i].first.c_str(), &_filters[i].second)) {
+                if(ImGui::Checkbox(_filters[i].first, &_filters[i].second)) {
                     _cached = false;
                 }
                 if(i < _filters.size() - 1) {
@@ -166,11 +166,11 @@ namespace World {
             }
 
             // Display current region
-            if(currRegion) [[likely]] {
+            if(currRegion) {
                 ImGui::Text("Current Region:");
                 ImGui::SameLine();
-                ImGui::TextColored(ImVec4(0.6f, 0.8f, 1.0f, 1.0f), "%s", _regionNames[currRegion].c_str());
-            } else [[unlikely]] {
+                ImGui::TextColored(ImVec4(0.6F, 0.8F, 1.0F, 1.0F), "%s", _regionNames[currRegion].c_str());
+            } else {
                 ImGui::Text("Current region not found");
             }
 
@@ -197,17 +197,20 @@ namespace World {
             // load list of plugins with available weathers
             std::unordered_set<RE::TESFile*> plugins;
             Utils::loadUsefulPlugins<RE::TESWeather>(plugins);
-            for(auto plugin : plugins) {
-                _mods.push_back(plugin);
-            }
+            // for(const auto* plugin : plugins) {
+            //     _mods.push_back(plugin);
+            // }
 
             const auto data{ RE::TESDataHandler::GetSingleton() };
             // load region&weather names
-            for(auto* weather : data->GetFormArray<RE::TESWeather>()) {
-                _weatherNames.insert({ weather, weather->GetFormEditorID() });
+            using GetFormEditorID = const char* (*)(std::uint32_t);
+            static auto po3_tweaks{ REX::W32::GetModuleHandle(L"po3_Tweaks") };
+            static auto get_form_editor_id{ reinterpret_cast<GetFormEditorID>(REX::W32::GetProcAddress(po3_tweaks, "GetFormEditorID")) };
+            for(const auto* weather : data->GetFormArray<RE::TESWeather>()) {
+                _weatherNames.insert({ weather, get_form_editor_id ? get_form_editor_id(weather->GetFormID()) : "NULL" });
             }
-            for(auto* region : data->GetFormArray<RE::TESRegion>()) {
-                _regionNames.insert({ region, region->GetFormEditorID() });
+            for(const auto* region : data->GetFormArray<RE::TESRegion>()) {
+                _regionNames.insert({ region, get_form_editor_id ? get_form_editor_id(region->GetFormID()) : "NULL" });
             }
         }
     }  // namespace Weather
@@ -215,7 +218,7 @@ namespace World {
     void show() {
         const auto sky{ RE::Sky::GetSingleton() };
         if(!sky || !sky->currentWeather) [[unlikely]] {
-            ImGui::Text("World not loaded");
+            ImGui::Text("World not yet loaded");
             return;
         }
         // Use consistent padding and alignment
